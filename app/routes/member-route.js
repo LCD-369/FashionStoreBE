baf13b9ac56bd8ba9982df75e767e772d2b4f27c
Louis Diaz <louis_c_diaz@outlook.com>
2020-05-07 22:34:41 -0400

    Tenth Commit

--- app/routes/member-route.js
+++ app/routes/member-route.js
@@ -6,7 +6,9 @@ const uuidv4 = require('uuid/v4');
 const router = express.Router();
 const checkAuth = require("../middleware/check-auth");
 const AWS = require('aws-sdk');
-AWS.config.update({region: 'us-east-1'});
+AWS.config.update({
+  region: 'us-east-1'
+});
 const tableName = 'FashionrusStore';
 const indexName = 'SK-index';
 docClient = new AWS.DynamoDB.DocumentClient();
@@ -36,11 +38,35 @@ router.get('/api/members', (req, res, next) => {
 
 //Create/add new member
 router.post('/api/member', (req, res, next) => {
-  let item = req.body.Item;
-  docClient.put({
+
+  let pk = req.body.PK
+  sk = req.body.SK,
+    email = req.body.email,
+    firstname = req.body.firstname,
+    lastname = req.body.lastname,
+    addressstreet = req.body.addressstreet,
+    city = req.body.city,
+    state = req.body.state,
+    country = req.body.country,
+    phone = req.body.phone,
+    zipcode = req.body.zipcode;
+  var params = {
     TableName: tableName,
-    Item: item
-  }, (err, data) => {
+    Item: {
+      "PK": pk,
+      "SK": sk,
+      "EMAIL": email,
+      "FIRSTNAME": firstname,
+      "LASTNAME": lastname,
+      "ADDRESSSTREET": addressstreet,
+      "CITY": city,
+      "STATE": state,
+      "COUNTRY": country,
+      "PHONE": phone,
+      "ZIPCODE": zipcode
+    }
+  };
+  docClient.put(params, (err, data) => {
     if (err) {
       console.log(err);
       return res.status(err.statusCode).send({
@@ -48,7 +74,56 @@ router.post('/api/member', (req, res, next) => {
         status: err.statusCode
       });
     } else {
-      return res.status(200).send(item);
+      return res.status(200).send(data);
+    }
+  });
+});
+
+router.put('/api/member/update', (req, res, next) => {
+  let pk = req.body.PK,
+    sk = req.body.SK,
+    firstname = req.body.firstname,
+    lastname = req.body.lastname,
+    addressstreet = req.body.addressstreet,
+    city = req.body.city,
+    state = req.body.state,
+    country = req.body.country,
+    phone = req.body.phone,
+    zipcode = req.body.zipcode;
+
+  let params = {
+    TableName: tableName,
+    Key: {
+      "PK": pk,
+      "SK": sk
+    },
+    UpdateExpression: 'set FIRSTNAME = :FIRSTNAME, LASTNAME = :LASTNAME, ADDRESSSTREET = :ADDRESSSTREET, CITY = :CITY, #st = :val1, ZIPCODE = :ZIPCODE, COUNTRY = :COUNTRY, PHONE = :PHONE',
+    ExpressionAttributeValues: {
+
+      ':FIRSTNAME': firstname,
+      ':LASTNAME': lastname,
+      ':ADDRESSSTREET': addressstreet,
+      ':CITY': city,
+      ':val1': state,
+      ':ZIPCODE': zipcode,
+      ':COUNTRY': country,
+      ':PHONE': phone,
+    },
+    ExpressionAttributeNames: {
+      "#st": "STATE"
+    },
+    ReturnValues: "UPDATED_NEW"
+  };
+  docClient.update(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(err.statusCode).send({
+        message: err.message,
+        status: err.statusCode
+      });
+    } else {
+      console.log(data);
+      return res.status(200).send(data);
     }
   });
 });
